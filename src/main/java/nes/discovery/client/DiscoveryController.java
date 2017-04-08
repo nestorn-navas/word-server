705ffda72e0caf212377fe179264dc746c98deeb
Nestor Navas <nestor.navas@gmail.com>
2017-04-08 14:32:45 +0200

    First

---
+++ src/main/java/nes/discovery/client/DiscoveryController.java
@@ -0,0 +1,19 @@
+package nes.discovery.client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DiscoveryController {
+	
+	private String words = "subject-mirror,subject-mirror";
+	
+	@RequestMapping("/")
+	public @ResponseBody String getWord() {
+		String[] wordArray = words.split(",");
+		int i = (int) Math.round(Math.random() * (wordArray.length - 1));
+		return wordArray[i];
+	}
+
+}
